f833f59d9637577efc8cd200e65d2c89a4ea7f9d
Mayank <mayanktripathi150@gmail.com>
2026-02-07 14:34:57 +0530

    Login Script - V1

---
+++ Scripts/test_login.py
@@ -0,0 +1,9 @@
+from playwright.sync_api import Page
+
+
+def test_TC1(page:Page):
+
+    page.goto("https://practicetestautomation.com/practice-test-login/")
+    page.locator("#username").fill("student")
+    page.locator("#password").fill("Password123")
+    page.locator("#submit").click()
